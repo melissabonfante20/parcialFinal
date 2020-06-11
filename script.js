09ca275c3f6042bcb1192130b84f3b9ab052acf3
Johan <johansamperg@gmail.com>
2020-06-10 19:19:43 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+    /* Caja 01 */
+    $('#caja-01 a').mouseover(function(){
+        $('#caja-01 .info-interna').fadeIn(500);
+    });
+    
+    $('#caja-01 a').mouseleave(function(){
+        $('#caja-01 .info-interna').fadeOut(300);
+    });
+    
+    /* Caja 02 */
+    $('#caja-02 a').mouseover(function(){
+        $('#caja-02 .info-interna').fadeIn(500);
+    });
+    
+    $('#caja-02 a').mouseleave(function(){
+        $('#caja-02 .info-interna').fadeOut(300);
+    });
+    
+    
+    /*
+    $('#caja-03 a').mouseover(function(){
+        
+    });
+    
+    $('#caja-04 a').mouseover(function(){
+        
+    });
+    
+    $('#caja-05 a').mouseover(function(){
+        
+    });
+    */
+    
+    $('#caja-01 a, #caja-02 a, #caja-03 a, #caja-04 a, #caja-05 a').click(function(){
+        event.preventDefault();
+    });
+    
+    
+    
+    /* --------------- */
+    
+    
+    $('a#ver-catalogo').click(function(){
+        $('figure#vista-02').animate({
+            left: 0
+        },700,'easeOutExpo')
+        
+        event.preventDefault();
+    });
+    
+    $('a#ver-infografia').click(function(){
+        $('figure#vista-02').animate({
+            left: '100%'
+        },700,'easeOutExpo')
+        
+        event.preventDefault();
+    });
+    
+    
+    /* ---------------- */
+    
+    
+    $('#flipbook').turn({
+        width: 900,
+        height: 530,
+        autoCenter: true,
+        gradients: true
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
